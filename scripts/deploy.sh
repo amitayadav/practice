130c05de8abf00aba45dcab48bce90dedafb2bbc
amita <amita.yadav@knoldus.in>
2018-08-30 13:46:03 +0530

    deploy changed

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,5 @@
 ps -ef | grep seed-1.0-SNAPSHOT | grep -v grep | awk '{print $2}' | xargs kill
+rm -rf seed-1.0-SNAPSHOT
 unzip seed-1.0-SNAPSHOT.zip
 ./seed-1.0-SNAPSHOT/bin/seed &
 rm -rf seed-1.0-SNAPSHOT.zip
